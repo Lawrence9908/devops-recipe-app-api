1aaec94dc15faea5e1d9d5585488cd4d7f54eb63
Lawrence9908 <mugwenalawrence121@gmail.com>
2024-12-26 15:55:55 +0200

    added terraform deploy files

---
+++ infra/deploy/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+  backend "s3" {
+    bucket               = "devops-recipe-app-tf-state-lawrence"
+    key                  = "tf-state-deploy"
+    workspace_key_prefix = "tf-state-deploy-env"
+    region               = "eu-north-1"
+    encrypt              = true
+    dynamodb_table       = "devops-recipe-app-api-tf-lock-lawrence"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.prefix
+      ManageBy    = "Terraform/deploy"
+    }
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+}
+
+data "aws_region" "current" {}
